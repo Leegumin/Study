e3e5479dfdb239b840f68ce81fcb4766d55d1a7a
Leegumin <littlegbear@naver.com>
2022-08-18 19:44:51 +0900

    2022-08-18

---
+++ JPA/강의/02_실습/jpa_member/src/main/java/com/example/jpa_member/JpaMemberApplication.java
@@ -0,0 +1,13 @@
+package com.example.jpa_member;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaMemberApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaMemberApplication.class, args);
+    }
+
+}
---
+++ JPA/강의/02_실습/jpa_member/src/test/java/com/example/jpa_member/JpaMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jpa_member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaMemberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
