e61bf2205e3f3d24ee06ff87a6c00b67eb29f562
Leegumin <littlegbear@naver.com>
2022-08-31 21:26:10 +0900

    2022-08-31

--- JPA/강의/02_실습/02_jpa_member/src/main/java/com/example/jpa_member/controller/JpaController.java
+++ JPA/강의/02_실습/02_jpa_member/src/main/java/com/example/jpa_member/controller/JpaController.java
@@ -1,8 +1,5 @@
 package com.example.jpa_member.controller;
 
-import com.example.jpa_member.dto.MemberDTO;
-import com.example.jpa_member.entity.Member;
-import com.example.jpa_member.repository.MemberRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,7 +11,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.List;
+import com.example.jpa_member.dto.MemberDTO;
+import com.example.jpa_member.entity.Member;
+import com.example.jpa_member.repository.MemberRepository;
 
 
 /**
