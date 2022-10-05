6328b05cab5f3071f2013e3b9dcfcb581b1197c2
GuminLee <littlegbear@naver.com>
2022-10-05 22:10:12 +0900

    2022-10-05

---
+++ JAVA/강의/02_실습/Practice_Chap01(환경설정테스트)/src/example_test/TestExample.java
@@ -0,0 +1,23 @@
+package example_test;
+
+/**
+ * packageName : example_test
+ * fileName : TestExample.java
+ * author : Mingu
+ * date : 2022. 9. 27.
+ * description : Java강의 Test Class
+ * ===========================================================
+ * DATE            AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2022. 9. 27.         Mingu          최초 생성
+ */
+
+public class TestExample {
+	// Ctrl + F11 : 컴파일 후 실행시키는 단축키
+	public static void main(String[] args) {
+		
+		System.out.println("안녕 자바");
+		
+	}
+
+}
